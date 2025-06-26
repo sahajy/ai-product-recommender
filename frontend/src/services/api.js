1fefc7fa8abfb07221d00195cb650b67b2c72a12
sahajy <sahajy330@gmail.com>
2025-06-26 19:29:54 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,3 +1,5 @@
+const API_URL = process.env.REACT_APP_API_URL || "https://ai-product-recommender-f2t3.onrender.com";
+
 export const fetchProducts = async () => {
   const response = await fetch('https://ai-product-recommender-f2t3.onrender.com/api/products');
   return response.json();
